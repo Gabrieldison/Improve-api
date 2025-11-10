202a53b7f5964140044462fe2df6ef3411461f1c
Gabriel dison <gabriel.disonreis@gmail.com>
2025-11-10 08:59:16 -0300

    feat: initial setup with database

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,30 @@
+import { pool } from "@/shared/infra/database/connection";
+import { app } from "@/shared/infra/http/app";
+
+const port = process.env.PORT || 3000;
+
+async function main() {
+  try {
+    const client = await pool.connect();
+    console.log("🟢 Database connected successfully");
+    client.release();
+
+    app.listen(port, () => {
+      console.log(`🚀 Server of improve is active and running on port ${port}`);
+    });
+
+    process.on("SIGINT", async () => {
+      console.log("\n🛑 Gracefully shutting down...");
+      await pool.end();
+      process.exit(0);
+    });
+  } catch (error) {
+    console.error(
+      "🔴 Failed to connect to the database:",
+      (error as Error).message
+    );
+    process.exit(1);
+  }
+}
+
+main();
